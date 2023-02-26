492c4f6951696ce53146deb34d033106adf1bc25
chintan-patel-developer <chintu20111999@gmail.com>
2023-02-26 22:48:14 +0530

    all done

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ for (const link of nav.getElementsByTagName("a")) {
   link.onmousemove = (e) => {
     const rect = link.getBoundingClientRect(),
       img = link.querySelector("img");
-
     img.style.left = `${e.clientX - rect.left}px`;
-    img.style.top = `${e.clientX - rect.top}px`;
+    img.style.top = `${e.clientY - rect.top}px`;
+    console.log(e.clientY);
+    console.log(rect.top);
+    console.log(img.style.top);
   };
 }
 
